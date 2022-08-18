e4a70bff61f723f65038df12f9d9f258983a1099
fajarajikusuma <fajikusuma@gmail.com>
2022-08-18 10:53:38 +0700

    add btn back to top

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ const target = document.querySelector('.topnav');
 menuSwitch.addEventListener('click', () => {
     target.classList.toggle('slide');
 });
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
